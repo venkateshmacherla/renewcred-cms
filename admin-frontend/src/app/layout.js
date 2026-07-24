059ae3710e073e64108a95e3a44ea31cead53ede
Venkatesh Macharla <macherlavenkatesh5@gmail.com>
2026-07-24 22:19:08 +0530

    chore: finalize RenewCred CMS for submission

--- admin-frontend/src/app/layout.js
+++ admin-frontend/src/app/layout.js
@@ -8,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
